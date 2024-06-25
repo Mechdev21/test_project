dde75d37c113582355803ca77c30f0afea96640e
Orji Favour <favour@titanname.com>
2024-06-25 18:03:46 +0100

    test

---
+++ config/db.config.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const configVariables = require('./config');
+
+const mongooseDBConnection =  () => {
+    mongoose
+    .connect (configVariables.MONGO_URL)
+    .then ( () => {
+        console.log("Successfully connected to the MongoDB database.");
+    })
+    .catch ( (err) => {
+        console.error(err);
+        throw new Error('Cnnection Error')
+    })
+};
+
+module.exports = mongooseDBConnection;
